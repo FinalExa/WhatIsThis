8a96c0b669a7f0dca5a09e21569d2a33e6fdd158
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-02-25 23:09:18 +0100

    Add player + playerbasestats

---
+++ WhatIsThisProject/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] private float baseAttack;
+    [SerializeField] private float baseHealth;
+    [SerializeField] private float baseLevel;
+    public float BaseAttack { get; set; }
+    public float BaseHealth { get; set; }
+    public float BaseLevel { get; set; }
+
+    private void Start()
+    {
+        BaseAttack = baseAttack;
+        BaseHealth = baseHealth;
+        BaseLevel = baseLevel;
+        print(BaseAttack + " " + BaseHealth + " " + BaseLevel);
+    }
+}
