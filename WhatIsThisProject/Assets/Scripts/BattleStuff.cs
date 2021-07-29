4f56148ab4139b9156417455ce4e96c79369f679
Alex Silvestri <alexsilvestri.16@gmail.com>
2021-07-29 23:56:24 +0200

    Madness import

---
+++ WhatIsThisProject/Assets/Scripts/BattleStuff.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class BattleStuff : MonoBehaviour
+{
+    public bool CheckSpeed(int playerSpeed, int enemySpeed)
+    {
+        bool isPlayerTurn = false;
+        if (playerSpeed >= enemySpeed) isPlayerTurn = true;
+        return isPlayerTurn;
+    }
+    /* <<widget "hitOrMiss">>
+    <<if random(1, 100) <= $miss >>
+        << set $miss = 0 >>
+
+
+     <<else>>
+
+
+         << set $miss = 1 >>
+
+
+      <</if>>
+  <</ widget >>
+
+  << widget "calculateDamage" >>
+
+
+       << set $puredamage = $atk* 15 + $wpn* 7.5 >>
+ 
+
+
+           << set $puredefense = $def* 10 + $arm* 5 >>
+  
+
+
+               << set $dmg = Math.round(Math.sqrt($puredamage - $puredefense) / 1.5) >>
+  
+
+
+                <<if $dmg< 1 >>
+  
+
+
+                    << set $dmg = 1 >>
+  
+
+
+                 <</if>>
+  
+             <</ widget >>
+
+             << widget "fightVictory" >>
+  
+
+
+                  << set $spoils = $enemy.level* 10 + (random($enemy.level* -2,$enemy.level* 2)) >>
+  
+
+
+                   << set $player.coins = $player.coins + $spoils >>
+  
+
+
+                    << set $expgain = $enemy.level* 50 + (random($enemy.level* -20,$enemy.level* 20)) >>
+  
+      << set $player.experience = $player.experience + $expgain >>
+   <</ widget >>*/
+}
---
+++ WhatIsThisProject/Assets/Scripts/ShopInventory.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class ShopInventory : MonoBehaviour
+{
+    /*<<set $villageShopInventory to[
+    {
+        name: "Old sword",
+        quality: 1,
+        quantity: 1,
+        type: "weapon",
+        value: 5,
+        price: 30
+    },
+    {
+name: "Old armor",
+        quality: 1,
+        quantity: 1,
+        type: "armor",
+        value: 5,
+        price: 50
+    },
+    {
+name: "Healing potion",
+        quality: 1,
+        quantity: -1,
+        type: "healing",
+        value: 10,
+        price: 10
+    }
+
+<<widget "shop">>
+    <<set $length = $inventoryShop.length>>
+    <<if $shopIndex< 0>><<set $shopIndex = $length - 1>><</if>>
+    <<if $shopIndex > $length - 1>><<set $shopIndex = 0>><</if>>
+    <<if $player.coins >= $inventoryShop[$shopIndex].price>>
+        <<if $inventoryShop[$shopIndex].quantity >= 1>>
+            $inventoryShop[$shopIndex].name(x$inventoryShop[$shopIndex].quantity) - <<link[[Buy |$nameShop]]>>
+                <<set $inventoryShop[$shopIndex].quantity = $inventoryShop[$shopIndex].quantity - 1>>
+                <<set $player.coins = $player.coins - $inventoryShop[$shopIndex].price>>
+                <<set $checkerFlag = 0>>
+                <<for $checker = 0; ($checker< $inventory.length) && ($checkerFlag == 0); $checker++>>
+                    <<if $inventoryShop[$shopIndex].name == $inventory[$checker].name>>
+                        <<set $checkerFlag = 1>>
+                        <<set $inventory[$checker].quantity++>>
+                    <</if>>
+                <</for>>
+                <<if $checkerFlag == 0>>
+                    <<set $invTempAdd to
+    {
+        name: $inventoryShop [$shopIndex].name,
+                        quality: $inventoryShop [$shopIndex].quality,
+                        quantity: 1,
+                        type: $inventoryShop [$shopIndex].type,
+                        value: $inventoryShop [$shopIndex].value
+    }>>
+                    <<set $inventory.push($invTempAdd)>>
+                <</if>>
+            <</link>> - $inventoryShop[$shopIndex].price coins
+        <<elseif $inventoryShop[$shopIndex].quantity == -1>>
+            $inventoryShop[$shopIndex].name - <<link[[Buy |$nameShop]]>>
+                <<set $player.coins = $player.coins - $inventoryShop[$shopIndex].price>>
+                <<set $checkerFlag = 0>>
+                <<for $checker = 0; ($checker< $inventory.length) && ($checkerFlag == 0); $checker++>>
+                    <<if $inventoryShop[$shopIndex].name == $inventory[$checker].name>>
+                        <<set $checkerFlag = 1>>
+                        <<set $inventory[$checker].quantity++>>
+                    <</if>>
+                <</for>>
+                <<if $checkerFlag == 0>>
+                    <<set $invTempAdd to
+    {
+        name: $inventoryShop [$shopIndex].name,
+                        quality: $inventoryShop [$shopIndex].quality,
+                        quantity: 1,
+                        type: $inventoryShop [$shopIndex].type,
+                        value: $inventoryShop [$shopIndex].value
+    }>>
+                    <<set $inventory.push($invTempAdd)>>
+                <</if>>
+            <</link>> - $inventoryShop[$shopIndex].price coins
+        <<elseif $inventoryShop[$shopIndex].quantity == 0>>
+            $inventoryShop[$shopIndex].name - The shop is out of stock of this item
+        <</if>>
+    <<else>>
+        $inventoryShop[$shopIndex].name - You can't afford that ($inventoryShop[$shopIndex].price coins)
+    <</if>><br>
+    <<link[[Next Element |$nameShop]]>><<set $shopIndex = $shopIndex + 1 >><</ link >>< br >
+    << link[[Previous Element |$nameShop]]>><< set $shopIndex = $shopIndex - 1 >><</ link >>
+  <</ widget >>
+]>> */
+}
